c2d0255be895eb46f396f2c1c63551b4c228c997
BogdanPazin <168831585+BogdanPazin@users.noreply.github.com>
2024-10-17 14:39:03 +0200

    Update cart.service.ts

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -9,10 +9,7 @@ export class CartService {
 
   cartItems: CartItem[] = [];
 
-  // SUBJECT JE PODKLASA OBSERVABLE KLASE GDE MOGU DA KORISTIM SUBJECT DA OBJAVI "DOGADJAJE", I TI "DOGADJAJI" CE BITI PRIMLJENI KOD SVIH SUBSCRIBER-A, PRI CEMU SUBSCRIBER-I PRIMAJU NOVE 'DOGADJAJE' NAKON STO SE SUBSCRIBE-UJU
-
-  // PRI CEMU POSTOJE 2 PODKLASE SUBJECT KLASE, REPLAYSUBJECT OMOGUCAVA DA SUBSCRIBER-I PRIME SVE DOGADJAJE KOJI SU SE DESILI (CAK I ONE KOJI SU BILI PRE NEGO STO SU SE OVI SUBSCRIBE-OVALI), I BEHAVIOURSUBJECT KOJI OMOGUCAVA DA SUBSCRIBER-I PRIME POSLEDNJI DOGADJAJ KOJI SE DESIO PRE NEGO STO SU SE ONI SUBSCRIBE-OVALI
-  totalPrice: Subject<number> = new BehaviorSubject<number>(0); // 0 JE POCETNA VREDNOST ZA ATRIBUT
+  totalPrice: Subject<number> = new BehaviorSubject<number>(0);
 
   totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
 
@@ -22,7 +19,6 @@ export class CartService {
 
   addToCart(cartItem: CartItem){
     let alreadyExistsInCart: boolean = false;
-    // MORAO SAM DA DODAM '!' KOD UNDEFINED-A DA NAZNACIM DA NECE BITI NULL
     let existingCartItem: CartItem = undefined!;
 
     if(this.cartItems.length > 0){
@@ -32,10 +28,6 @@ export class CartService {
           break;
         }
       }
-
-      // KORISTIM find() METODU KOJA JE MOGUCA ZA SVE NIZOVE KOJA PROLAZI KROZ SVE ELEMENTE I ISPITUJE ODREDJENI USLOV, SAMA METODA VRACA PRVI ELEMENT KOJI ISPUNJAVA ODREDJENI USLOV ILI INACE VRACA undefined
-      
-      // existingCartItem = this.cartItems.find(tempItem => tempItem.id == cartItem.id)!;
 
       alreadyExistsInCart = (existingCartItem != undefined);
     }
@@ -61,7 +53,6 @@ export class CartService {
       totalQuantityValue = totalQuantityValue + cartItem.quantity;
     }
 
-    // OVIM CE DA SE OBJAVE VREDNOSTI ZA UKUPNU CENU I ZA UKUPAN BROJ PROIZVODA I SVI SUBSCRIBER-I CE DA PRIHVATE TE VREDNOSTI
     this.totalPrice.next(totalPriceValue);
     this.totalQuantity.next(totalQuantityValue);
 
@@ -91,11 +82,9 @@ export class CartService {
   }
 
   remove(cartItem: CartItem) {
-    // VRACA INDEKS PRVOG PODUDARANJA SA ODREDJENIM USLOVOM, AKO SE NIJEDAN NE PODUDARA ONDA VRACA -1
     const index = this.cartItems.findIndex(tmpItem => tmpItem.id == cartItem.id);
 
     if(index > -1){
-      // OVOM METODOM BRISEM ELEMENT IZ NIZA NA ODREDJENOM INDEKSU, PRI CEMU SAM NAZNACIO DA BRISEM SAMO JEDAN ELEMENT IZ NIZA
       this.cartItems.splice(index, 1);
 
       this.computeCartTotals();
