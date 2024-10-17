b06069aa4da0ce88ad37ac14f4c9c9bb51b09591
BogdanPazin <168831585+BogdanPazin@users.noreply.github.com>
2024-10-17 14:55:56 +0200

    Update purchase.ts

--- src/app/common/purchase.ts
+++ src/app/common/purchase.ts
@@ -3,7 +3,6 @@ import { Customer } from "./customer";
 import { Order } from "./order";
 import { OrderItem } from "./order-item";
 
-// GLAVNI OBJEKAT ZA SLANJE PODATAKA IZ CHECKOUT FORME PREMA REST API
 export class Purchase {
     customer: Customer = undefined!;
     shippingAddress: Address = undefined!;
